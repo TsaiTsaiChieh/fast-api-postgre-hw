65b796a767bd7cebf5a9f4130e399bffca2f0099
d0177381@gmail.com <>
2021-01-19 19:48:30 +0800

    [Add] login_log table

--- app/db/schemas.py
+++ app/db/schemas.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Sequence, Column, Integer, String, Boolean, DateTime, create_engine
+from sqlalchemy import Sequence, Column, Integer, String, Boolean, DateTime, create_engine, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from app import settings
@@ -19,8 +19,16 @@ class User(Base):
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
     
-    # def __init__(self, name, account, password):
-    #     self.name = name
-    #     self.account = account
-    #     self.password = password
-
+    def __repr__(self):
+        return f"User(name={self.name}, account={self.account}, active={self.active})"
+class LoginLog(Base):
+    __tablename__ = "login_log"
+    
+    login_log_id_seq = Sequence("login_log_id_seq")
+    id = Column(Integer, login_log_id_seq, server_default=login_log_id_seq.next_value(), primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    login_ip = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    
+    def __repr__(self):
+        return f"User(user_id={self.user_id}, login_ip={self.login_ip}, created_at={self.created_at})"
