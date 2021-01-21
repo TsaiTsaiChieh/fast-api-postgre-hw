6942e93a3dd83f9b871b5cfbe52325a3c37ed861
d0177381@gmail.com <>
2021-01-21 15:52:36 +0800

    [Add] AuthJWT setting

--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,17 @@
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     app_name: str = "fast-api-postgre-hw"
     api_prefix: str = "/api/v1"
     database_url: str = "postgresql://postgres:0000@localhost:5432/fast-api-postgre-hw"
-    # jwt settings
-    # to get a string like this run: openssl rand -hex 32
-    secret_key: str = "8087ae2e8f8df45cfa05ea70492a30733acee81e4112d3e9ea943207c378be5c"
-    algorithm: str = "HS256"
+    # AuthJWT
     access_token_expired_minutes: int = 30
-    
-settings = Settings()
+    authjwt_secret_key: str = (
+        "8087ae2e8f8df45cfa05ea70492a30733acee81e4112d3e9ea943207c378be5c"
+    )
+    authjwt_denylist_enabled: bool = True
+    authjwt_denylist_token_checks: set = {"access", "refresh"}
 
+
+settings = Settings()
