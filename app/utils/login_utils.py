0fb10bce45d9a6b04cc81ed897fadee091557f4b
d0177381@gmail.com <>
2021-01-21 15:54:53 +0800

    [Change] format

--- app/utils/login_utils.py
+++ app/utils/login_utils.py
@@ -1,17 +1,22 @@
-#Standard
-#Project
+# Standard
+# Project
 from db.connection import session
 from db.schemas import User, LoginLog
 from utils.schemas import UserRegisterSchema, LoginLogSchema
 
+
 def getUserInfoByAccount(account: str):
     return session.query(User).filter(User.account == account).first()
 
+
 def createUserItem(user: UserRegisterSchema):
-    data = User(name=user.name, account=user.account, password=user.password, active=True)
+    data = User(
+        name=user.name, account=user.account, password=user.password, active=True
+    )
     session.add(data)
     session.commit()
-    
+
+
 def createLoginLogs(loginLog: LoginLogSchema):
     data = LoginLog(user_id=loginLog["user_id"], login_ip=loginLog["login_ip"])
     session.add(data)
