fcb653e128ce952e10b8b10de5e4fbcbf5e176d1
d0177381@gmail.com <>
2021-01-21 15:55:32 +0800

    [Change] use fastapi_jwt_auth module

--- app/utils/verify.py
+++ app/utils/verify.py
@@ -1,40 +1,37 @@
-#Standard
+# Standard
 from typing import Optional
 from datetime import datetime, timedelta
 from jose import JWTError, jwt, ExpiredSignatureError
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
-#Project
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+
+# Project
 from app import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-SECRET_KEY = settings.settings.secret_key
-ALGORITHM = settings.settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.settings.access_token_expired_minutes
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta]=None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+def createAccessToken(
+    data: dict,
+    Authorize: AuthJWT = Depends(),
+    expiredDelta: Optional[int] = None,
+) -> str:
+    if expiredDelta:
+        expire = timedelta(expiredDelta)
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encode_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
-    return encode_jwt
-    
-def verify_token(token: str):
-    try:
-        return jwt.decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
-    except (JWTError, ExpiredSignatureError):
-        raise e
+        expire = timedelta(minutes=settings.settings.access_token_expired_minutes)
+    encodeJWT = Authorize.create_access_token(subject=str(data), expires_time=expire)
+    return encodeJWT
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    print(token)
-    credentials_exception = HTTPException(
+
+def verifyToken(Authorize: AuthJWT = Depends()):
+    credentialsException = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
+        detail="Missing token or access token expired",
     )
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    return payload
+    try:
+        Authorize.jwt_required()
+        payload = Authorize.get_jwt_subject()
+    except AuthJWTException:
+        raise credentialsException
+    return eval(payload)
